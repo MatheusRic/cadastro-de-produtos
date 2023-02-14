6b63a95962ff0da2090eacd391def11c06a990d7
Matheus Ricardo <matheu_ric99@hotmail.com>
2023-02-14 16:22:18 -0300

    feat: adicionando busca por param

--- src/controllers/products/listProducts.controller.ts
+++ src/controllers/products/listProducts.controller.ts
@@ -2,7 +2,8 @@ import { Response, Request } from "express";
 import listProductsService from "../../services/products/listProducts.service";
 
 const listProductsController = async (req: Request, res: Response) => {
-  const products = await listProductsService();
+  const param = req.query["true"];
+  const products = await listProductsService(param);
   return res.status(200).json(products);
 };
 
--- src/services/products/listProducts.service.ts
+++ src/services/products/listProducts.service.ts
@@ -1,10 +1,15 @@
 import AppDataSource from "../../data-source";
 import Products from "../../entities/products.entity";
 
-const listProductsService = async (): Promise<Products[]> => {
+const listProductsService = async (param: any): Promise<Products[]> => {
   const productRepository = AppDataSource.getRepository(Products);
 
-  const products = await productRepository.find({ withDeleted: true });
+  if (param) {
+    const products = await productRepository.find({ withDeleted: true });
+    return products;
+  }
+
+  const products = await productRepository.find();
 
   return products;
 };
